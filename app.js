6a2824856ee7ae2cc48c7ed0642c83cf173a2b34
hassaniko <elsawyhassan2@gmail.com>
2022-01-06 16:40:37 +0200

    setting up main app file for routing

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hello world!");
+const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({path:'./.env'});
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+
+db.connect((err) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("MYSQL Connected ...");
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1> Home Page</h1>")
+})
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000")
+})
