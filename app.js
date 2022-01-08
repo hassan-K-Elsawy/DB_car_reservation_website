2e623b7fc5e406836d26e66de04a0955c06b136e
hassaniko <elsawyhassan2@gmail.com>
2022-01-09 01:43:46 +0200

    first implementation of store

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
-const path = require('path')
+const path = require('path');
+const router = express.Router();
 
 dotenv.config({path:'./.env'});
 
--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 router.get("/", (req, res) => {
     res.render('index');
@@ -10,7 +18,37 @@ router.get("/register", (req, res) =>{
 });
 
 router.get("/Login", (req, res) =>{
-    res.render('login') // TODO implement login.hbs 
+    res.render('login') 
 })
+
+router.get("/store", (req, res) => {
+    db.query('SELECT * FROM car WHERE status = "available" ', (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            return res.render("store", {
+                cars: results 
+            });
+        }
+    });
+});
+
+router.post("/cart", (req, res) =>{
+    const carid = req.body.car;
+    db.query('SELECT * FROM car WHERE plateID = ?',[carid] ,(error, results) =>{
+        if(error){
+                console.log(error);
+        }else{
+            //console.log(results);
+            res.render("cart", {
+                car: results[0]
+            });  
+        }   
+    })
+});
+
+
+
+
 
 module.exports = router;
